a941d684542c40b8417312e2e7b87bc67622ea0e
Morrizon14 <diegoandml@gmail.com>
2024-08-10 15:37:57 +0200

    modify conexion

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,31 +1,28 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
 
 
-try{
+
 //La variable "conn" permitirá crear una conexión con nnuestra BD.
-//$conn = mysqli_connect('localhost','root','','imfca');
+$conn = mysqli_connect('localhost','root','','imfca');
 
-$conn = new PDO("mysql:host=$servername;dbname=imfca", $username, $password);
+//$conn = new PDO("mysql:host=$servername;dbname=imfca", $username, $password);
 
 
-// Verificar si la conexión fue exitosa
-//if (!$conn) {
+ //Verificar si la conexión fue exitosa
+if (!$conn) {
   
-  //echo "Error en la conexion a la base de datos: ";
-  //exit();
-//}
-
-// Establecer la codificación de caracteres
-//mysqli_set_charset($conn, "utf8");
-
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+  echo "Error en la conexion a la base de datos: ";
+  exit();
 }
+
+//Establecer la codificación de caracteres
+mysqli_set_charset($conn, "utf8");
+
+//$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //echo "Connected successfully";
+//} catch(PDOException $e) {
+  //echo "Connection failed: " . $e->getMessage();
+//}
 
 ?>
